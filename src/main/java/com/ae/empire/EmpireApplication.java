88f5ad6508d2d4fde2140c0270af59d0abb13d55
Andy Valjakka <110393708+aendyeh-c@users.noreply.github.com>
2022-08-25 13:16:43 +0300

    Spring initializr

---
+++ src/main/java/com/ae/empire/EmpireApplication.java
@@ -0,0 +1,13 @@
+package com.ae.empire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ae/empire/EmpireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ae.empire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
