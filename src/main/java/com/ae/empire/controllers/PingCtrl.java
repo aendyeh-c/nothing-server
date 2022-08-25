36779553b3a6ec7182beb3aa8ca63d04e003255a
Andy Valjakka <110393708+aendyeh-c@users.noreply.github.com>
2022-08-25 13:26:27 +0300

    Create PingCtrl.java

---
+++ src/main/java/com/ae/empire/controllers/PingCtrl.java
@@ -0,0 +1,18 @@
+package com.ae.empire.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class PingCtrl {
+
+    @GetMapping("/ping")
+    public ResponseEntity<Map> ping() {
+        return ResponseEntity.ok(Map.of("status", "OK"));
+    }
+}
