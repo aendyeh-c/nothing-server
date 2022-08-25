3256860807fa870456a64641b0304d5e53062bca
Andy Valjakka <110393708+aendyeh-c@users.noreply.github.com>
2022-08-25 13:54:15 +0300

    Env response

--- src/main/java/com/ae/empire/controllers/IdentityCtrl.java
+++ src/main/java/com/ae/empire/controllers/IdentityCtrl.java
@@ -1,8 +1,12 @@
 package com.ae.empire.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/self/v1")
@@ -10,5 +14,13 @@ public class IdentityCtrl {
 
   @Value("${app.env}")
   private String env;
-  
+
+  @GetMapping()
+  public Map envGetter() {
+    return Map.of(
+      "env",
+      env
+    );
+  }
+
 }
