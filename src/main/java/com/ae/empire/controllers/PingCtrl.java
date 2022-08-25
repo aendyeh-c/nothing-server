f9ea3f2530d8b02f4107fd36114c7a7d8f1fdeee
Andy Valjakka <110393708+aendyeh-c@users.noreply.github.com>
2022-08-25 13:34:11 +0300

    Kill endpoint

--- src/main/java/com/ae/empire/controllers/PingCtrl.java
+++ src/main/java/com/ae/empire/controllers/PingCtrl.java
@@ -1,5 +1,6 @@
 package com.ae.empire.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,17 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class PingCtrl {
 
   @GetMapping("/ping")
   public ResponseEntity<Map> ping() {
     return ResponseEntity.ok(Map.of("status", "OK"));
+  }
+
+  @GetMapping("/kill")
+  public void kill() {
+    log.warn("I have decided that I want to die");
+    while (true) {}
   }
 }
